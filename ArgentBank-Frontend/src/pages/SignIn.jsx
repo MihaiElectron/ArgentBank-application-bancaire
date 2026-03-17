09b57b3c71af8010c459298f6b52b4061f255066
Mihai Radulescu <mihaielectron@gmail.com>
2026-03-17 20:13:31 +0100

    feat(ui): add SignIn page with isolated CSS and set #root as flex container

--- ArgentBank-Frontend/src/pages/SignIn.jsx
+++ ArgentBank-Frontend/src/pages/SignIn.jsx
@@ -1,13 +1,32 @@
-// Page de connexion utilisateur
-// Correspond au fichier "sign-in.html" du dossier Frontend-Ref.
-// Cette page contiendra le formulaire de connexion + interaction avec Redux + API backend.
+import "./SignIn.css";
 
 export default function SignIn() {
   return (
-    <main>
-      {/* TODO : Intégrer le formulaire HTML/CSS depuis sign-in.html */}
-      <h1>Sign In</h1>
-      <p>Formulaire de connexion à intégrer ici.</p>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+
+          {/* EXACTEMENT comme le HTML original */}
+          <a href="/user" className="sign-in-button">Sign In</a>
+        </form>
+      </section>
     </main>
   );
 }
