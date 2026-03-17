7762fed934ae3558a39061ab466a75253f273539
Mihai Radulescu <mihaielectron@gmail.com>
2026-03-17 20:39:18 +0100

    feat(ui): add Sign Out link to Header

--- ArgentBank-Frontend/src/components/Header.jsx
+++ ArgentBank-Frontend/src/components/Header.jsx
@@ -1,8 +1,4 @@
 // Header.jsx
-// Reproduction fidèle du header HTML fourni dans ArgentBank-Frontend-Ref.
-// Contient : logo, lien vers la home, lien Sign In avec icône FontAwesome.
-// Le CSS utilisé est celui de main.css (déjà présent dans le dossier de référence).
-
 import { Link } from "react-router-dom";
 import argentBankLogo from "../assets/argentBankLogo.png"; 
 import "./Header.css";
@@ -20,11 +16,14 @@ export default function Header() {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
 
-      {/* Zone de droite : Sign In (ou User + Logout plus tard via Redux) */}
+      {/* Zone de droite : version user.html → User + Sign Out */}
       <div>
-        <Link className="main-nav-item" to="/sign-in">
-          <i className="fa fa-user-circle"></i>{" "}
-          Sign In
+        <Link className="main-nav-item" to="/user">
+          <i className="fa fa-user-circle"></i> Tony{" "}
+        </Link>
+
+        <Link className="main-nav-item" to="/">
+          <i className="fa fa-sign-out"></i> Sign Out
         </Link>
       </div>
     </nav>
