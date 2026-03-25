13bd7daa37425f0ed59d6fe107bdca5b44ab415a
Mihai Radulescu <mihaielectron@gmail.com>
2026-03-25 11:51:14 +0100

    feat(state): initialize Redux Toolkit store

--- ArgentBank-Frontend/src/main.jsx
+++ ArgentBank-Frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // Ajout : active le système de routing
+import { Provider } from "react-redux";
+import { store } from './store/store';
 import './styles/reset.css'
 import App from './App.jsx'
 
@@ -8,7 +10,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* BrowserRouter permet à React Router de gérer les URL */}
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
